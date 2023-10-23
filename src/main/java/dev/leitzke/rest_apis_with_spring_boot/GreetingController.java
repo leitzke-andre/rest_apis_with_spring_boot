87158f229746f2b7036c942f0ac024ee373301ac
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2023-10-23 15:45:40 +0200

    Initial adjustments to startup class name, changed application from .properties to .yaml and implemented a greeter.

---
+++ src/main/java/dev/leitzke/rest_apis_with_spring_boot/GreetingController.java
@@ -0,0 +1,24 @@
+package dev.leitzke.rest_apis_with_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private static final String templatePT = "Olá, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "World") String name,
+            @RequestParam(value = "lang", defaultValue = "en") String language) {
+        if(language.equalsIgnoreCase("pt")) {
+            return new Greeting(counter.incrementAndGet(), String.format(templatePT, name));
+        }
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
