9d353549441f1354e78fadcb31f0f8d17ef900ad
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2023-10-23 14:27:46 +0200

    Imported base project from spring initializr

---
+++ src/main/java/dev/leitzke/rest_apis_with_spring_boot/RestApisWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.leitzke.rest_apis_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApisWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApisWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leitzke/rest_apis_with_spring_boot/RestApisWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leitzke.rest_apis_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApisWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
