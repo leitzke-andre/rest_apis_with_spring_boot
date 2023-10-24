456f540e39a73eee9e800144439ff84d9688bcc1
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2023-10-24 10:38:09 +0200

    Removed "greeting" code and implemented MathController

---
+++ src/main/java/dev/leitzke/rest_apis_with_spring_boot/MathController.java
@@ -0,0 +1,21 @@
+package dev.leitzke.rest_apis_with_spring_boot;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+    @RequestMapping(value = "/sum/{firstOperand}/{secondOperand}", method = RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "firstOperand") String firstOperand,
+            @PathVariable(value = "secondOperand") String secondOperand
+    ) {
+        return convertToDouble(firstOperand) + convertToDouble(secondOperand);
+    }
+
+    private Double convertToDouble(String number) {
+        return Double.valueOf(number);
+    }
+
+}
