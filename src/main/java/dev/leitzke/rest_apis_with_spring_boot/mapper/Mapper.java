02228794606e1d3d755d74b55f4b8b7fdb81bc82
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2023-10-27 10:57:21 +0200

    Removed unnecessary dependencies.

--- src/main/java/dev/leitzke/rest_apis_with_spring_boot/mapper/Mapper.java
+++ src/main/java/dev/leitzke/rest_apis_with_spring_boot/mapper/Mapper.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public class Mapper {
 
-    private static ModelMapper modelMapper = new ModelMapper();
+    private static final ModelMapper modelMapper = new ModelMapper();
 
     public static <O, D> D mapObject(O origin, Class<D> destination){
         return modelMapper.map(origin, destination);
     }
 
     public static <O, D> List<D> mapListObjects(List<O> origin, Class<D> destination) {
-        List<D> destinationObjects = new ArrayList<D>();
+        List<D> destinationObjects = new ArrayList<>();
         for (O o: origin
         ) {
             destinationObjects.add(modelMapper.map(o, destination));
