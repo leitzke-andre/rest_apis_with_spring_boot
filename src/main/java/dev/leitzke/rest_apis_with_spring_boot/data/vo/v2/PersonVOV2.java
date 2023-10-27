51bf51724707c37a1fc4c813798153b85ed315dd
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2023-10-27 11:40:53 +0200

    Added VO v2 to begin implementing V2 API.

---
+++ src/main/java/dev/leitzke/rest_apis_with_spring_boot/data/vo/v2/PersonVOV2.java
@@ -0,0 +1,96 @@
+package dev.leitzke.rest_apis_with_spring_boot.data.vo.v2;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+public class PersonVOV2 implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    public PersonVOV2() {
+    }
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+    private LocalDate birthDate;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonVOV2 that = (PersonVOV2) o;
+
+        if (!id.equals(that.id)) return false;
+        if (!firstName.equals(that.firstName)) return false;
+        if (!lastName.equals(that.lastName)) return false;
+        if (!address.equals(that.address)) return false;
+        if (!gender.equals(that.gender)) return false;
+        return birthDate.equals(that.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + address.hashCode();
+        result = 31 * result + gender.hashCode();
+        result = 31 * result + birthDate.hashCode();
+        return result;
+    }
+}
